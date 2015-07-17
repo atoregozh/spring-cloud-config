302e377a37d1500ae5ee6c6d7ff28bf608a933ab
Aizhan <toregozh@gmail.com>
2015-07-17 10:04:15 -0600

    fetching from new environment

--- configclient/src/main/java/demo/MainApp.java
+++ configclient/src/main/java/demo/MainApp.java
@@ -1,10 +1,12 @@
 package demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainApp {
     @Value("${bar:World!}")
     String bar;
-
+    @Autowired Environment springEnv;
     @RequestMapping("/")
     String hello() {
+        System.out.println(springEnv);
         return "Hello " + bar + "!";
     }
+
 
 
     public static void main(String[] args) {
